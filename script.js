31d9bdd9bff77b2763a8063e4b2a3d42aca54eda
poohtime <138019702+poohtime@users.noreply.github.com>
2024-03-09 23:27:57 +0900

    수정

--- script.js
+++ script.js
@@ -28,3 +28,7 @@ buckets.forEach((bucket, index) => {
 
 // 오디오 소리 default 값 설정
 document.getElementById("background_music").volume = 0.2;
+
+const addBucketList = () => {
+    const bucketList = document.getElementById("bucket_list");
+}
